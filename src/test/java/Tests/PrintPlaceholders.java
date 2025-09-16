5b78b430d0bdc022c2cc06d8aec3f0a66ba65550
Mustafa Sabra <mustafasabra44@gmail.com>
2025-09-16 03:10:00 +0300

    Adding the PrintPlaceholders class and test

---
+++ src/test/java/Tests/PrintPlaceholders.java
@@ -0,0 +1,20 @@
+package Tests;
+
+import TestBase.TestBase;
+import org.testng.annotations.Test;
+
+public class PrintPlaceholders extends TestBase {
+
+
+
+    @Test
+    public void printPlaceholders()
+    {
+        subscriptionForm.printPlaceholders();
+    }
+
+
+
+
+
+}
